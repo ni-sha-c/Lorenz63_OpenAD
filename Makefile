ba081be59b8fbee53d149bedcb5071588468dab4
Nisha Chandramoorthy <nishac92@gmail.com>
2017-06-02 10:04:01 -0500

    Lorenz'63 complete example, adjoint mode

--- Makefile
+++ Makefile
@@ -4,16 +4,28 @@ endif
 ifndef CC
 CC=gcc
 endif
-RTSUPP=w2f__types OAD_active OAD_cp OAD_tape OAD_rev 
-driver: $(addsuffix .o, $(RTSUPP) iaddr) driver.o head.prepped.pre.xb.x2w.w2f.post.o
+RTSUPP=w2f__types OAD_active OAD_cp OAD_tape OAD_rev ad_inline  
+driver: $(addsuffix .o, $(RTSUPP) iaddr)  lorenz63_passive.o numCore.pre.xb.x2w.w2f.post.o driver.o
 	${F90C} -o $@ $^
-head.prepped.pre.xb.x2w.w2f.post.f90 $(addsuffix .f90, $(RTSUPP)) iaddr.c : toolChain 
-toolChain : head.prepped.f90
+numCore.pre.xb.x2w.w2f.post.f90 $(addsuffix .f90, $(RTSUPP)) iaddr.c : toolChain 
+toolChain : numCore.f90
 	openad -c -m rj $<
+
+numCore.f90: Lorenz63.f90 head.prepped.f90
+	cat $^ > $@
+
+lorenz63_passive.f90: Lorenz63.f90
+	cat $^ | sed 's/Lorenz63/lorenz63_passive/' > $@
+  
+
+ad_inline.f:toolChain
+
 %.o : %.f90
 	${F90C} -o $@ -c $< 
 %.o : %.c
 	${CC} -o $@ -c $< 
+
+
 clean: 
 	rm -f ad_template* ad_inline.f OAD_* w2f__*  iaddr* 
 	rm -f head.prepped.pre.* *.B *.xaif *.o *.mod driver driverE *~ 
--- driver.f90
+++ driver.f90
@@ -1,14 +1,48 @@
 program driver
-  use OAD_active
-  use OAD_rev
-  implicit none 
-  external head
-  type(active) :: x, y
-  x%v=2.8D1
-  y%d=1.0D0
-  our_rev_mode%tape=.TRUE.
-  call head(x,y)
-  print *, 'driver running for x =',x%v
-  print *, '            yields y =',y%v,' dy/dx =',x%d
-  print *, '    dz^1000/dz = ',1.D0*cos(x%v)-x%d
+	use OAD_active
+	use OAD_rev
+	use Lorenz63
+	implicit none 
+	external head
+	type(active) :: x, y
+	real(kind=8), dimension(3) :: X0
+	real(kind=8), dimension(3,1) :: v0,v
+	real(kind=8) :: dzds_fd
+	integer :: t
+	real(kind=8) :: zs, z0
+	x%v=2.8D1
+	y%d=1.0D0
+	our_rev_mode%tape=.TRUE.
+	call head(x,y)
+	print *, 'driver running for x =',x%v
+	print *, '            yields y =',y%v,' dy/dx =',x%d
+
+	!check using tangent equation
+	X0(1) = -2.4d0
+	X0(2) = -3.7d0
+	X0(3) = 14.98d0
+	v0(1,1) = 0.d0
+	v0(2,1) = 0.d0
+	v0(3,1) = 0.d0
+	do t = 1, 1000, 1
+		call rk45_full(X0,v0,v)
+		call Xnp1(X0,X0,x%v)
+		v0 = v
+	end do	
+
+	z0 = X0(3)
+	dzds_fd = v(3,1)
+	print *,'dy/dx from the tangent equation is: ', dzds_fd
+
+	!Check using finite difference	
+	X0(1) = -2.4d0
+	X0(2) = -3.7d0
+	X0(3) = 14.98d0
+
+	do t = 1, 1000, 1
+		call Xnp1(X0,X0,x%v+0.0005)
+	end do	
+	zs = X0(3)
+	print *,'dy/dx from finite difference is: ', (zs-z0)/0.0005
+	print *, '    dz^1000/dz = ',1.D0*dzds_fd-x%d
 end program driver
